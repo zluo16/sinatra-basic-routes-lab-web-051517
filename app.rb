7cd78ab1e73da74a324d7cce24ae3484c35c0810
zluo16 <zluo@alumni.berklee.edu>
2017-06-05 13:02:00 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get ('/name'){"My name is Mike"}
+  get ('/hometown'){"My hometown is Manalapan, NJ"}
+  get ('/favorite-song'){"My favorite song is Big Poppa"}
+
 end
